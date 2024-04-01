14267226323e798bba587e6ce14754d8aaefe621
milou2764 <emilien.andre2764@gmail.com>
2024-04-01 16:56:01 +0200

    gmod does not like uppercases

--- mrpds.sh
+++ mrpds.sh
@@ -6,7 +6,7 @@ SCRIPT_PATH="$(readlink -f $_)"
 SCRIPT_DIR="$(dirname $SCRIPT_PATH)"
 GAME_DIR="$SCRIPT_DIR/garrysmod"
 REPOS="$GAME_DIR/gamemodes/militaryrp;https://github.com/milou2764/militaryrp
-	$GAME_DIR/addons/bolemosAiO;https://github.com/reskihed0311/bolemosAiO
+	$GAME_DIR/addons/bolemosaio;https://github.com/reskihed0311/bolemosAiO
 	$GAME_DIR/addons/tompms;https://github.com/milou2764/tompms"
 
 for r in $REPOS
